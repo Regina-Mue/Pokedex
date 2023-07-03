55e75c1ad5905a73ab3f248613a093c5af48a04a
Regina-Mue <regina.muelller97@gmail.com>
2023-07-03 20:03:35 +0200

    Adding Pokemon to pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,52 @@
-let pokemonList = [];
+let pokemonList = [
+    arceus = {
+        name: 'Arceus',
+        height: 3.2,
+        types: ['normal']
+    },
+    mewtwo = {
+        name: 'Mewtwo',
+        height: 2,
+        types: ['psychic']
+    },
+    giratina = {
+        name: 'Giratina',
+        height: 4.5,
+        types: ['dragon', 'ghost']
+    },
+    dialga = {
+        name: 'Dialga',
+        height: 5.4,
+        types: ['dragon', 'steel']
+    },
+    palkia = {
+        name: 'Palkia',
+        height: 4.2,
+        types: ['dragon', 'water']
+    },
+    rayquaza = {
+        name: 'Rayquaza',
+        height: 7,
+        types: ['dragon', 'flying']
+    },
+    groudon = {
+        name: 'Groudon',
+        height: 3.5,
+        types: ['ground']
+    },
+    kyogre = {
+        name: 'Kyogre',
+        height: 4.5,
+        types: ['water']
+    },
+    ho_oh = {
+        name: 'Ho-oh',
+        height: 3.8,
+        types: ['fire', 'flying']
+    },
+    celebi = {
+        name: 'Celebi',
+        height: 0.6,
+        types: ['psychic', 'grass']
+    },
+];
