4083915ed5950f6ea4f6ab64dee14d8b9122fb5b
Regina-Mue <regina.muelller97@gmail.com>
2023-07-05 00:03:35 +0200

    add a condition

--- js/scripts.js
+++ js/scripts.js
@@ -52,5 +52,11 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    
+    // Check the pokemons height
+    if (pokemonList[i].height > 5.5) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!<br>');
+    } else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<br>');
+    }
 }
