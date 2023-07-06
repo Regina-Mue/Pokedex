5841d80f2eafc63d4648ba7b51aa8ceaadf6d36d
Regina-Mue <regina.muelller97@gmail.com>
2023-07-06 20:36:52 +0200

    Put pokemonList in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,63 @@
-let pokemonList = [
-    {   
-        name: 'Arceus',
-        height: 3.2,
-        types: ['normal']
-    },
-    {   
-        name: 'Mewtwo',
-        height: 2,
-        types: ['psychic']
-    },
-    {   
-        name: 'Giratina',
-        height: 4.5,
-        types: ['dragon', 'ghost']
-    },
-    {
-        name: 'Dialga',
-        height: 5.4,
-        types: ['dragon', 'steel']
-    },
-    {
-        name: 'Palkia',
-        height: 4.2,
-        types: ['dragon', 'water']
-    },
-    {
-        name: 'Rayquaza',
-        height: 7,
-        types: ['dragon', 'flying']
-    },
-    {
-        name: 'Groudon',
-        height: 3.5,
-        types: ['ground']
-    },
-    {
-        name: 'Kyogre',
-        height: 4.5,
-        types: ['water']
-    },
-    {
-        name: 'Ho-oh',
-        height: 3.8,
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Celebi',
-        height: 0.6,
-        types: ['psychic', 'grass']
-    },
-];
+(function () {
+
+    let pokemonList = [
+        {   
+            name: 'Arceus',
+            height: 3.2,
+            types: ['normal']
+        },
+        {   
+            name: 'Mewtwo',
+            height: 2,
+            types: ['psychic']
+        },
+        {   
+            name: 'Giratina',
+            height: 4.5,
+            types: ['dragon', 'ghost']
+        },
+        {
+            name: 'Dialga',
+            height: 5.4,
+            types: ['dragon', 'steel']
+        },
+        {
+            name: 'Palkia',
+            height: 4.2,
+            types: ['dragon', 'water']
+        },
+        {
+            name: 'Rayquaza',
+            height: 7,
+            types: ['dragon', 'flying']
+        },
+        {
+            name: 'Groudon',
+            height: 3.5,
+            types: ['ground']
+        },
+        {
+            name: 'Kyogre',
+            height: 4.5,
+            types: ['water']
+        },
+        {
+            name: 'Ho-oh',
+            height: 3.8,
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Celebi',
+            height: 0.6,
+            types: ['psychic', 'grass']
+        },
+    ];
+  
+    return {
+      pokedex: pokemonList
+    };
+    
+})();
 
 pokemonList.forEach(function(pokemon) {
     if (pokemon.types.length == 2) {
